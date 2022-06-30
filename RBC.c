c123cf6736b212a9eb6bb5998a6342c14f58e5fe
alienix2 <mattemoni@gmail.com>
2022-06-30 02:20:56 +0200

    Prelimiari per RBC  e Registro, ancora di test

--- RBC.c
+++ RBC.c
@@ -1,1 +1,81 @@
-//Interamente da scrivere per bene
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h> /* For AFUNIX sockets */
+#define DEFAULT_PROTOCOL 0
+
+//RBC di PROVA, ToDo quello vero
+void leggiElemento (int fd, char *str) {   //Funzione generica per ricevere un numero non definito di caratteri
+    do {
+        while(recv (fd, str, 1, 0) < 0){
+            sleep(2);
+       }
+    }while (*str++ != '\0');
+}
+
+void creaSocket(int *server, struct sockaddr_un *serverAddress, int *serverLen, char* nomeServer){  //Funzione che crea una socket partendo dai dati che riceve come parametri
+    *serverLen = sizeof(*serverAddress);
+    *server = socket(AF_UNIX, SOCK_STREAM, 0);
+    struct sockaddr_un tmp = {.sun_family = AF_UNIX};
+    strcpy (tmp.sun_path, nomeServer);
+    *serverAddress = tmp;
+}
+
+void connetti(int server, struct sockaddr *serverAddressPtr, int serverLen){   //Funzione che crea una connessione coi dati che riceve come parametri
+    int risultato;
+    do { /* Loop until a connection is made with the server */
+        risultato = connect (server, serverAddressPtr, serverLen);
+        if (risultato == -1){
+	        sleep (3); /* Wait and then try again */
+        }
+    } while (risultato == -1);
+}
+
+
+int receiveNumero(int fd) {
+    int numero;
+    recv(fd, &numero, sizeof(numero), 0);
+    numero = ntohl(numero);
+    printf("Ho ricevuto %d, PID:%d\n", numero, getpid());
+}
+
+void riceviItinerari (int fd, char **itinerario[6][7]) {  //Ricevo 7 stringhe e le metto correttamente all'interno dell'itinerario del treno
+    char str[200];
+    int i = 0;
+    char *prova[7];
+    for(int k = 1; k<6; k++){
+        for(int i = 0; i<7; i++){
+            leggiElemento(fd, str);
+            itinerario[k][i] = (char*) malloc(strlen(str)*sizeof(char));
+            strcpy(itinerario[k][i], str);
+        }
+    }
+}
+
+void inviaNumero(int fd){  //Invia un numero sul canale
+    int numero = htonl(0);
+    while(send(fd, &numero, sizeof(numero), 0) < 0){
+        sleep(2);
+    }
+}
+
+int main (void) {
+    int registro, registroLen;
+    int clientFd, serverLen, clientLen;
+    struct sockaddr* serverSockAddrPtr; /*Ptr to server address*/
+    struct sockaddr_un clientUNIXAddress; /*Client address */
+
+    struct sockaddr_un registroAddress; //Indirizzo del registro
+    struct sockaddr* registroAddressPtr = (struct registroAddressPtr*) &registroAddress; //Puntatore all'indirizzo del registro
+
+    char *itinerario[6][7];
+    creaSocket(&registro, &registroAddress, &registroLen, "RegistroTreni");
+    connetti(registro, registroAddressPtr, registroLen);
+    inviaNumero(registro);
+    riceviItinerari(registro, &itinerario);
+    printf("%s\n", itinerario[1][6]);
+    return 0;
+}
--- registro.c
+++ registro.c
@@ -2,62 +2,72 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h> /* For AFUNIX sockets */
+#include <arpa/inet.h>
 #define DEFAULT_PROTOCOL 0
 
 //Registro di PROVA, ToDo quello vero
 
-int receiveNumero(int fd) {
-  int numero;
-  recv(fd, &numero, sizeof(numero), 0);
-  numero = ntohl(numero);
-  printf("Ho ricevuto %d, PID:%d\n", numero, getpid());
+int receiveNumero(int fd, int *numeroTreno) {
+    recv(fd, numeroTreno, sizeof(*numeroTreno), 0);
+    *numeroTreno = ntohl(*numeroTreno);
+    printf("Ho ricevuto %d, PID:%d\n", *numeroTreno, getpid());
 }
 
-int sendItinerario(int fd, char **itinerario){
-  for(int i = 0; i<7; i++){
-    send(fd, itinerario[i], strlen(itinerario[i])+1, 0);
-  }
-  fflush(fd);
-  return 0;
+int sendItinerario(int fd, char **itinerario[5][7], int numeroTreno){
+    if(numeroTreno == 0){
+        for(int k = 1; k<6; k++){
+            for(int i = 0; i<7; i++){
+                send(fd, itinerario[1][i], strlen(itinerario[1][i])+1, 0); //DA MODIFICARE 1 CON k
+            }
+        }
+    }
+    else{
+        numeroTreno = 1; //DA ELIMINARE DOPO QUANDO SI METTONO LE MATRICI GIUSTE
+        for(int i = 0; i<7; i++){
+            send(fd, itinerario[numeroTreno][i], strlen(itinerario[numeroTreno][i])+1, 0);
+        }
+    }
+    fflush(fd);
+    return 0;
 }
 
 int main (void) {
-  int registro, clientFd, serverLen, clientLen;
-  struct sockaddr_un registroAddress; /*Server address */
-  struct sockaddr* serverSockAddrPtr; /*Ptr to server address*/
-  struct sockaddr_un clientUNIXAddress; /*Client address */
-  struct sockaddr* registroAddressPtr;/*Ptr to client address*/
+    int registro, clientFd, serverLen, clientLen;
+    struct sockaddr_un registroAddress; /*Server address */
+    struct sockaddr* serverSockAddrPtr; /*Ptr to server address*/
+    struct sockaddr_un clientUNIXAddress; /*Client address */
+    struct sockaddr* registroAddressPtr;/*Ptr to client address*/
+    int numeroTreno;
 
-  char *itinerario[7];
-  int random = rand();
-  itinerario[0] = "S0";
-  itinerario[1] = "MA1";
-  itinerario[2] = "MA2";
-  itinerario[3] = "MA3";
-  itinerario[4] = "MA4";
-  itinerario[5] = "MA5";
-  itinerario[6] = "S1";
+    char *itinerario[6][7];
+    itinerario[1][0] = "S0";
+    itinerario[1][1] = "MA1";
+    itinerario[1][2] = "MA2";
+    itinerario[1][3] = "MA3";
+    itinerario[1][4] = "MA4";
+    itinerario[1][5] = "MA5";
+    itinerario[1][6] = "S1";
 
-  serverSockAddrPtr = (struct sockaddr*) &registroAddress;
-  serverLen = sizeof (registroAddress);
-  registroAddressPtr = (struct sockaddr*) &clientUNIXAddress;
-  clientLen = sizeof (clientUNIXAddress);
+    serverSockAddrPtr = (struct sockaddr*) &registroAddress;
+    serverLen = sizeof (registroAddress);
+    registroAddressPtr = (struct sockaddr*) &clientUNIXAddress;
+    clientLen = sizeof (clientUNIXAddress);
   
-  registro = socket (AF_UNIX, SOCK_STREAM, DEFAULT_PROTOCOL);
-  registroAddress.sun_family = AF_UNIX; /* Set domain type */
-  strcpy (registroAddress.sun_path, "RegistroTreni"); /* Set name */
-  unlink ("RegistroTreni"); /* Remove file if it already exists */
-  bind (registro, serverSockAddrPtr, serverLen);/*Create file*/
-  listen (registro, 5); /* Maximum pending connection length */
-  while (1) {/* Loop forever */ /* Accept a client connection */
-    clientFd = accept (registro, registroAddressPtr, &clientLen);
-    if (fork () == 0) { /* Create child to send receipe */
-      receiveNumero(clientFd);
-      sendItinerario(clientFd, &itinerario);
-    } else
-      close (clientFd); /* Close the client descriptor */
-  }
+    registro = socket (AF_UNIX, SOCK_STREAM, DEFAULT_PROTOCOL);
+    registroAddress.sun_family = AF_UNIX; /* Set domain type */
+    strcpy (registroAddress.sun_path, "RegistroTreni"); /* Set name */
+    unlink ("RegistroTreni"); /* Remove file if it already exists */
+    bind (registro, serverSockAddrPtr, serverLen);/*Create file*/
+    listen (registro, 5); /* Maximum pending connection length */
+    while (1) {/* Loop forever */ /* Accept a client connection */
+        clientFd = accept (registro, registroAddressPtr, &clientLen);
+        if (fork () == 0) { /* Create child to send receipe */
+            receiveNumero(clientFd, &numeroTreno);
+            sendItinerario(clientFd, &itinerario, numeroTreno); //Da mettere al posto di 0 numeroTreno
+        }else close(clientFd);/* Close the client descriptor */
+    }
 }
